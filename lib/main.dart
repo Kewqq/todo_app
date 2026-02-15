bf3f4658ac6945090cd5bebc7ebf18fba5d6b1ca
Thatsanachai Sangthong 047 <rojer.007kew@gmail.com>
2026-02-15 22:50:13 +0700

    ver. 0.0.6

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'screens/home_screen.dart'; // เรียกใช้หน้า Home
+import 'screens/home_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/signup_register.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // เชื่อมต่อ Firebase
+
+  // --- แก้ไขตรงนี้ ---
+  try {
+    await Firebase.initializeApp();
+    print("Firebase เชื่อมต่อสำเร็จ!");
+  } catch (e) {
+    print("Firebase Error: $e");
+    // ถึง Error ก็ปล่อยผ่านไปก่อน เพื่อให้หน้าจอขึ้น
+  }
+
+  // ย้าย runApp มาไว้นอกสุด เพื่อให้ทำงานเสมอ
   runApp(const MyApp());
 }
 
@@ -14,14 +26,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // ปิดป้าย Debug มุมขวาบน
-      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter To-Do',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFFF5555)),
+        primarySwatch: Colors.red,
         useMaterial3: true,
-        fontFamily: 'Roboto', // (ถ้ามีฟอนต์)
       ),
-      home: const NewHomeScreen(), // เรียกหน้าแรก
+      home: const LoginScreen(),
     );
   }
 }
